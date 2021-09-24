c41e925312df5fe75429f32f023688d0eba436f9
btcain4 <82231147+btcain44@users.noreply.github.com>
2021-09-23 21:22:28 -0600

    Update Assignment_3_Code.R

--- Assignment_3/Assignment_3_Code.R
+++ Assignment_3/Assignment_3_Code.R
@@ -19,13 +19,31 @@ z_test <- function(x1, x2) {
   x2_bar = mean(x2)
   
   ##Compute sample standard deviation
-  x1_std = var(x1)^2
-  x2_std = var(x2)^2
+  x1_std = var(x1)**2
+  x2_std = var(x2)**2
   
   ##Calculate the z-statistic
   z = (x1_bar-x2_bar)/sqrt(x1_std/n1+x2_std/n2)
-  
+  return(x1_bar)
   ##Conditional to assess if the feature is significant
-  if (z<1.96) {return('Yes')} else {return('no')}
+  #if (z<1.96) {return('Yes')} else {return('no')}
 }
 ##Help with the function: https://cran.r-project.org/web/packages/distributions3/vignettes/two-sample-z-test.html
+
+##Perform for loop to test if features are significant with the defined z_test function
+covariates = list(names(dat))
+covariates = covariates[covariates != 'spam'] ##Filter out response variable
+for (i in covariates) {
+  
+  ##Create vectors
+  x1 = dat[which(dat$spam==0),i]
+  x2 = dat[which(dat$spam==1),i]
+  
+  ##Perform z-test
+  #z_result = z_test(x1,x2)
+  
+  ##Display z-test results
+  #print(paste(i,': ',z_result))
+  
+}
+
